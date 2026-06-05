35229c68fdeea679863e999210ca1ce06bd292bc
Eduputa ifechukwu <eduputaifechukwu@gmail.com>
2026-06-05 13:56:15 +0100

    Update dependencies

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const { CloudinaryStorage } = require("multer");
 const cloudinary = require("../config/cloudinary");
 
 const verifyAdmin = require("../middleware/verifyAdmin");
