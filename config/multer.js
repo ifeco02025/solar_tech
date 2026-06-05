e552d67f49b3ba50ad17bd4c03d2b12a2fa41cc3
Eduputa ifechukwu <eduputaifechukwu@gmail.com>
2026-06-05 14:05:15 +0100

    Fix CloudinaryStorage import

--- config/multer.js
+++ config/multer.js
@@ -1,5 +1,5 @@
 const multer = require("multer");
-const { CloudinaryStorage } = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("./cloudinary");
 
 // const storage = new CloudinaryStorage({
